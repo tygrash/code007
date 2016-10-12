6ce790caf4b9f2a647a4d92f47c8942fa64246ad
Ashwin Sinha <ashwin.sinha@go-jek.com>
2016-10-12 20:31:33 +0530

    Introducing cars in parkingLot. Failing test case.

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -1,14 +1,18 @@
 import java.util.HashMap;
 
 public class ParkingLot {
-	private Integer n;
+	private Integer slotsCount;
 	
 	private HashMap<Integer, Car> parkingLotMapping = new HashMap<>();
 	
 	public ParkingLot(Integer n) {
-		this.n = n;
+		this.slotsCount = n;
 		for (int i = 1; i <= n; i++) {
 			this.parkingLotMapping.put(i, null);
 		}
+	}
+	
+	public Integer getSlotsCount() {
+		return this.slotsCount;
 	}
 }
--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -7,4 +7,26 @@ public class ParkingLotTest {
        ParkingLot parkingLot = new ParkingLot(6);
        assertEquals(ParkingLot.class, parkingLot.getClass());
     }
+    
+    @Test
+    public void testParkingLotObjectClassNegativeTest() {
+    	ParkingLot parkingLot = new ParkingLot(6);
+    	assertNotEquals(Car.class, parkingLot.getClass());
+    }
+    	
+    @Test
+    public void testParkingLotEntriesCount() {
+    	ParkingLot parkingLot = new ParkingLot(6);
+    	assertEquals(new Integer(6), parkingLot.getSlotsCount());
+    }
+    
+    @Test
+    public void testCarsInParkingSlots() {
+    	ParkingLot parkingLot = new ParkingLot(6);
+    	Car carOne = new Car("MH-12 12-222", "White");
+    	parkingLot.insert(carOne);
+    	Car carTwo = new Car("MH-01 01-299", "Black");
+    	parkingLot.insert(carTwo);
+    	assertEquals(new Integer(2), parkingLot.totalCars());
+    }
 }
